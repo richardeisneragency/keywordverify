c70c3115eec1ea00e1be638d13c5b8bcecf5146f
Richard Eisner <eisner2020@Richards-MacBook-Air-2.local>
2025-01-14 20:53:26 -0500

    Update API service to use environment variable for API URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,31 +1,30 @@
 import axios from 'axios';
 import { Client } from '../types';
-
-const API_URL = 'http://localhost:3000/api';
+import { API_URL } from '../config';
 
 export const api = {
   getClients: async () => {
-    const response = await axios.get<Client[]>(`${API_URL}/clients`);
+    const response = await axios.get<Client[]>(`${API_URL}/api/clients`);
     return response.data;
   },
 
   addClient: async (client: Client) => {
-    const response = await axios.post<Client>(`${API_URL}/clients`, client);
+    const response = await axios.post<Client>(`${API_URL}/api/clients`, client);
     return response.data;
   },
 
   updateClient: async (client: Client) => {
-    const response = await axios.put<Client>(`${API_URL}/clients/${client.id}`, client);
+    const response = await axios.put<Client>(`${API_URL}/api/clients/${client.id}`, client);
     return response.data;
   },
 
   deleteClient: async (clientId: string) => {
-    const response = await axios.delete(`${API_URL}/clients/${clientId}`);
+    const response = await axios.delete(`${API_URL}/api/clients/${clientId}`);
     return response.data;
   },
 
   checkKeywords: async () => {
-    const response = await axios.post(`${API_URL}/check-keywords`);
+    const response = await axios.post(`${API_URL}/api/check-keywords`);
     return response.data;
   }
 };
